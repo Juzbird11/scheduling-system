1f870db528b044f844a0a3b92462d176deeb5207
juzbird11 <juzbird11@gmail.com>
2024-05-02 23:22:16 +0700

    first commit

---
+++ src/test/java/juz/bird/schedulingsystem/SchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package juz.bird.schedulingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
